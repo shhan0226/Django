7d853ca25a366154893d0694a31ee32f1615a8a1
shhan0226 <shhan0226@gmail.com>
2020-11-29 05:34:35 -0800

    2020-11-29 10:34

--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('my_to_do_app.urls')),
+    path('admin/', admin.site.urls),
 ]
--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import*
 
 def index(request):
 	#return HttpResponse("my_to_do_app first padge")
         return render(request, 'my_to_do_app/index.html')
+
+def createTodo(request):
+        user_input_str = request.POST['todoContent']
+        
+        # DB에 저장
+        new_todo = Todo(content = user_input_str)
+        new_todo.save()
+
+        return HttpResponse("create ToDo !! =>" + user_input_str )
