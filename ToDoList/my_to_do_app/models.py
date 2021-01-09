b5fae962517a3bf1e30d81b31c4a45a0b104ff9c
shhan0226 <shhan0226@gmail.com>
2021-01-09 09:22:11 -0800

    2021-01-09

--- ToDoList/my_to_do_app/models.py
+++ ToDoList/my_to_do_app/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class Todo(models.Model):
-    content = models.CharField(max_length = 255)
+	content = models.CharField(max_length = 255)
--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
 from .models import*
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 
 def index(request):
-	#return HttpResponse("my_to_do_app first padge")
-        return render(request, 'my_to_do_app/index.html')
+        todos = Todo.objects.all()
+        content = {'todos': todos}
+        return render(request, 'my_to_do_app/index.html', content)
 
 def createTodo(request):
         user_input_str = request.POST['todoContent']
-        
-        # DB에 저장
         new_todo = Todo(content = user_input_str)
         new_todo.save()
+        return HttpResponseRedirect(reverse('index'))
+#        return HttpResponse("create ToDo !! =>" +user_input_str )
 
-        return HttpResponse("create ToDo !! =>" + user_input_str )
+
+def doneTodo(request):
+        done_todo_id = request.GET['todoNum']
+        print("완료한 todo의 id",done_todo_id)
+        todo = Todo.objects.get(id = done_todo_id)
+        todo.delete()
+        return HttpResponseRedirect(reverse('index'))
